d11d01e9b4cfbddcfd0787a142accbb9a3423d17
anilin <anilinr@gmail.com>
2022-05-17 17:28:03 +0200

    Disabled karate integration tests due to not being able to run maven build with an embedded server, and no equivalent of Spring's @Profile exists in Jakarta-EE? Will discuss this more in-depth.

--- src/test/java/com/somecompany/textprocessing/karate/KarateTest.java
+++ src/test/java/com/somecompany/textprocessing/karate/KarateTest.java
@@ -4,17 +4,17 @@ import com.intuit.karate.junit5.Karate;
 
 public class KarateTest {
 
-    @Karate.Test
+//    @Karate.Test
     Karate wordFrequency() {
         return Karate.run("word-frequency").relativeTo(getClass());
     }
 
-    @Karate.Test
+//    @Karate.Test
     Karate highestFrequency() {
         return Karate.run("highest-frequency").relativeTo(getClass());
     }
 
-    @Karate.Test
+//    @Karate.Test
     Karate mostFrequentNWords() {
         return Karate.run("most-frequent-n-words-In-text").relativeTo(getClass());
     }
